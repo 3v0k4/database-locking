153d5078581f970874a73c0831d7680090d11d71
3v0k4 <riccardo.odone@gmail.com>
2017-07-18 15:18:58 +0200

    use syms instead of strings

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  scope "read_write" do
+  scope :read_write do
     get :fetch, to: "read_write#fetch"
     post :not_safe, to: "read_write#not_safe"
     post :safe, to: "read_write#safe"
   end
 
-  scope "write_write" do
+  scope :write_write do
     get :fetch, to: "write_write#fetch"
     post :not_safe, to: "write_write#not_safe"
     post :safe, to: "write_write#safe"
