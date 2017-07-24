89e62b59add88743f661b9656948230699271bac
3v0k4 <riccardo.odone@gmail.com>
2017-07-24 11:36:00 +0200

    concurrency.rb

---
+++ bin/concurrency.rb
@@ -0,0 +1,21 @@
+NUMBER_OF_RUNS = 5
+NUMBER_OF_THREADS = 5
+NUMBER_OF_INCREMENTS = 100_000
+
+NUMBER_OF_RUNS.times do |i|
+  puts "run ##{i}"
+
+  counters = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+
+  NUMBER_OF_THREADS.times.map do
+    Thread.new do
+      NUMBER_OF_INCREMENTS.times do
+        counters.map! { |counter| counter + 1 }
+      end
+    end
+  end.each(&:join)
+
+  puts counters.join(" ")
+  puts
+end
+
